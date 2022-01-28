53c5896584119e5287e243cd990934774fd6b709
Klemmerik <Klemmerik@gmx.de>
2022-01-28 03:23:31 +0100

    add ScreenRecorder.sh

---
+++ ScreenRecorder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#select current screen size
+screen_size=$(xrandr | grep '*' | awk '{print $1}')
+
+#select current audio output source
+audio_source=$(pacmd list-sources | grep output | head -n 1 | awk '{print $2}')
+
+#remove first and last character from audio_output_source string
+audio_source=$(echo ${audio_source} | cut -c 2- | rev | cut -c 2- | rev)
+
+#take current date
+current_date=$(date +%d.%m.%Y)
+
+
+ffmpeg -s ${screen_size} -framerate 25 -f x11grab -i :0.0 -f pulse -i ${audio_source} video.mp4 &>/dev/null
+
+
